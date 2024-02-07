e8bf9cd28156e9f28d3e06f91a91891111464635
MaciejDudziak <maciejdudziak777@gmail.com>
2024-02-07 11:05:36 -0500

    added first model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class exercise(models.Model):
+    name = models.CharField(max_length=100)
+    rep_number = models.IntegerField()
+    series_number = models.IntegerField()
