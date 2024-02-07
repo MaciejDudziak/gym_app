3f4351339ef97392a03e790224ba93e03988cf6f
MaciejDudziak <maciejdudziak777@gmail.com>
2024-02-07 15:14:32 -0500

    updated variable name

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse
 from .models import exercise
 # Create your views here.
 def home(request):
-    items = exercise.objects.all()
-    return render(request, "home.html", {"exercises": items})
+    exercises = exercise.objects.all()
+    return render(request, "home.html", {"exercises": exercises})
