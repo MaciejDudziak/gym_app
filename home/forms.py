f091a415d9396722df15d6c3dfb82e2f6718803a
MaciejDudziak <maciejdudziak777@gmail.com>
2024-02-26 22:36:08 -0500

    created register form for users at homepage

---
+++ home/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(label="username",max_length="150")
+    email = forms.EmailField(label="email",max_length="150")
+    password = forms.CharField(label="password", widget=forms.PasswordInput())
+        
--- home/views.py
+++ home/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from .forms import RegisterForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 # Create your views here.
 
 def home(request):
-    return HttpResponse("home")
+    
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            try:
+                user = User.objects.create_user(username,email,password)
+                user.save()
+                return HttpResponse("User created sucessfully")
+            except IntegrityError:
+                return HttpResponse("User already exists")
+    
+    else:
+        form = RegisterForm()
+
+    return render(request, "home.html", {"form":form})
 
 def login(request):
     return HttpResponse("login")
