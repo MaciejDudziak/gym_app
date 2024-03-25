77320a5e91e6cb03e20631a967486c7b3d3bd6c9
MaciejDudziak <maciejdudziak777@gmail.com>
2024-03-25 08:11:00 -0400

    added log in template

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect
-from .forms import RegisterForm
+from .forms import RegisterForm, LogInForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 # Create your views here.
@@ -9,7 +9,27 @@ def home(request):
     return render(request, "home.html")
 
 def login(request):
-    return HttpResponse("login")
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = LogInForm(request.POST)
+
+            if form.is_valid():
+                username = form.cleaned_data["username"]
+                email = form.cleaned_data["email"]
+                password = form.cleaned_data["password"]
+                try:
+                    user = User.objects.create_user(username,email,password)
+                    user.save()
+                    return HttpResponse("User created sucessfully")
+                except IntegrityError:
+                    return HttpResponse("User already exists")
+        
+        else:
+            form = LogInForm()
+
+        return render(request, "register.html", {"form":form})
+    else:
+        return redirect('home')
 
 def register(request):
      
